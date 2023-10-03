0501c10a60e09afb76e5ba002714fdce1c0c5cda
Alberto Viale <albertoviale2015@gmail.com>
2023-10-03 19:08:09 -0300

    conexionData

--- src/AccesoADatos/ConexionData.java
+++ src/AccesoADatos/ConexionData.java
@@ -5,10 +5,45 @@
  */
 package AccesoADatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author alber
  */
 public class ConexionData {
-    
+
+
+
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String BD = "nutricionista";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection = null;     //variable de tipo Connection que es las que va a usar nuestras clases Data 
+                                                    //  a traves de un objeto de tipo connection para generar los PreparedStatement
+
+    private ConexionData() {        //constructor
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {                       //verifica que es la primera vez que se realiza la conexion atraves de la variable Connection
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");      //carga los driver de conexion
+                connection = DriverManager.getConnection(URL + BD, USUARIO, PASSWORD);      //se crear el objeto connection
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "No se puedo encontrar el Driver");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "No se puedo encontrar la base de datos");
+            }
+        }
+        return connection;      //fin conexion
+
+    }
 }
+
+    
+
